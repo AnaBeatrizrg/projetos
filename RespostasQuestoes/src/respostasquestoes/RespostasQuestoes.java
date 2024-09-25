67ea50141834cba770791fb62d94b734fa96a6f0
AnaBeatrizrg <143762291+AnaBeatrizrg@users.noreply.github.com>
2024-09-25 01:12:11 -0300

    Add files via upload

---
+++ RespostasQuestoes/src/respostasquestoes/RespostasQuestoes.java
@@ -0,0 +1,14 @@
+/* Respostas das questões 3 a 5 do teste técnico.
+3) 11.
+
+4)  a) 9, 11, 13.
+    b) 128, 256, 512.   
+    c) 49, 64, 81.
+    d)100, 144, 196. 
+    e) 13, 21, 34. 
+    f)20, 21, 22.
+
+5) Ligaria um dos interruptores e verificaria qual lâmpada está acesa. Depois voltava e desligava o interruptor e ligava outro interruptor pra ver qual a outra lampada que acende.
+O interruptor que sobrou fica com a lâmpada que não foi verificada.
+Também poderia ser verificado ligando uma lampada por um tempo, 5 minutos, desligava essa lâmpada e ligava outra. 
+Quando fosse vericiar as lâmpadas, a que estiver quente e desligada é do primeiro interruptor, a que estiver acesa do segundo, e a que estiver desligada e fria é a do terceiro. */
