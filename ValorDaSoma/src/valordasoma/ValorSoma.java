1612ffc96e505d18eb0e2118a5c168b94774c951
AnaBeatrizrg <143762291+AnaBeatrizrg@users.noreply.github.com>
2024-09-25 00:56:37 -0300

    Add files via upload

---
+++ ValorDaSoma/src/valordasoma/ValorSoma.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author dev
+ */
+public class ValorSoma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int indice = 12;
+       int k = 1; 
+       int soma = 0;
+       
+       while (k < indice){
+           k++; soma++;
+       System.out.println(soma);      
+    }
+    
+}
+}
+// int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA); 
